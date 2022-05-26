404ef70f42bfe941ae82db3502f996fe0d33d65e
Shun Isii <sisii00.swork@gmail.com>
2022-05-26 11:04:18 +0900

    もっとシンプルにしてみた

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 import aws_cdk as cdk
-from aws_cdk import Duration, aws_apigateway, aws_cloudwatch, aws_codedeploy, aws_lambda
+from aws_cdk import aws_apigateway, aws_cloudwatch, aws_codedeploy, aws_lambda
 
 app = cdk.App()
 stack = cdk.Stack(app, "test")
@@ -36,26 +36,12 @@ aws_apigateway.LambdaRestApi(stack, "Api", handler=alias)
 # 3-1. CodeDeployを作成する
 # アプリケーションを作る
 application = aws_codedeploy.LambdaApplication(stack, "Application")
-# 動作確認でなる早でデプロイが終わるように設定した(`$ code deploy`したときにターミナルが返ってこなくなるから)
-config = aws_codedeploy.CustomLambdaDeploymentConfig(
-    stack,
-    "Config",
-    type=aws_codedeploy.CustomLambdaDeploymentConfigType.CANARY,
-    interval=Duration.minutes(1),
-    percentage=50,
-)
-# hook用のLambdaをつくる（オプション）
-pre_hook_fn = aws_lambda.Function(stack, "PreHook", handler="hooks.pre_hook_handler", **FN_PARAM)
-post_hook_fn = aws_lambda.Function(stack, "PostHook", handler="hooks.post_hook_handler", **FN_PARAM)
-# デプロイメントグループを作る
 deployment_group = aws_codedeploy.LambdaDeploymentGroup(
     stack,
     "CanariaDeploy",
     application=application,
     alias=alias,
-    deployment_config=config,
-    pre_hook=pre_hook_fn,
-    post_hook=post_hook_fn,
+    deployment_config=aws_codedeploy.LambdaDeploymentConfig.CANARY_10_PERCENT_5_MINUTES
 )
 
 # 3-2. デプロイ中にエラーが発生した場合にロールバックさせる
